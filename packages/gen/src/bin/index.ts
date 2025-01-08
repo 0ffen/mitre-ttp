b338b8491d6173b6ef1cab20b151ef284f9c201a
박금혁 <ghpark@enki.co.kr>
2025-01-08 19:46:11 +0900

    wip

--- packages/gen/src/bin/index.ts
+++ packages/gen/src/bin/index.ts
@@ -1,4 +1,5 @@
 import arg from "arg";
+import { isLanguageCode } from "../utils/lang";
 
 const args = arg({});
 
@@ -18,11 +19,19 @@ switch (command) {
       console.error("Usage: gen translate <lang>");
       process.exit(1);
     }
+    if (!isLanguageCode(args._[1])) {
+      console.error("Invalid language code");
+      process.exit(1);
+    }
     import("../generate").then((m) => m.default(args._[1]));
     break;
   case "translate":
     if (!args._[1]) {
       console.error("Usage: gen translate <lang>");
+      process.exit(1);
+    }
+    if (!isLanguageCode(args._[1])) {
+      console.error("Invalid language code");
       process.exit(1);
     }
     import("../translate").then((m) => m.default(args._[1]));
